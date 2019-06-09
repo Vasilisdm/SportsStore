e7cb38b79f50ae60f23410b3b430c6ab244e1190
Vasilisdm <vasilisdmr@gmail.com>
2019-06-09 18:46:09 +0300

    Created a fake repository with fixed products for time being.

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product {Name = "Macbook Pro mid 2014", Price = 1438},
+            new Product {Name = "Macbook Pro mid 2015", Price = 1638},
+            new Product {Name = "Macbook Pro mid 2016", Price = 1838}
+        }.AsQueryable();
+    }
+}
