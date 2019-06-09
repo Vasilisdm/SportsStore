b7538489d950a932d06738adb6c2a7e154ceeb4a
Vasilisdm <vasilisdmr@gmail.com>
2019-06-09 22:33:45 +0300

    Created product controller with its constructor.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+    }
+}
