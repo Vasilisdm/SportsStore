36a961f85010a66274b1bc769dc66a398796f2b6
Vasilisdm <vasilisdmr@gmail.com>
2019-06-19 00:25:56 +0300

    Added a field and the constructor of PageLinkTagHelper class.

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
+
 namespace SportsStore.Infrastructure
 {
-    public class PageLinkTagHelper
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
     {
-        public PageLinkTagHelper()
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
         {
+            urlHelperFactory = helperFactory;
         }
     }
 }
