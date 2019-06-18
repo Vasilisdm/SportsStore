0094b5d9025260d06e5b7412a5e5bf06f1d9d81f
Vasilisdm <vasilisdmr@gmail.com>
2019-06-19 00:11:47 +0300

    Added necessary props to PagingInfo view model.

--- SportsStore/Models/ViewModels/PagingInfo.cs
+++ SportsStore/Models/ViewModels/PagingInfo.cs
@@ -3,8 +3,10 @@ namespace SportsStore.Models.ViewModels
 {
     public class PagingInfo
     {
-        public PagingInfo()
-        {
-        }
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
 }
