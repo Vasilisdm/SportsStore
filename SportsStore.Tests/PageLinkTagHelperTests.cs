59f3d48d14ad35d5256f0a2a37b68f8d4219a957
Vasilisdm <vasilisdmr@gmail.com>
2019-06-19 23:09:22 +0300

    Started the Can_Generate_Link method.

--- SportsStore.Tests/PageLinkTagHelperTests.cs
+++ SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+//using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using SportsStore.Infrastructure;
+using SportsStore.Models.ViewModels;
+using Xunit;
+
 namespace SportsStore.Tests
 {
     public class PageLinkTagHelperTests
     {
-        public PageLinkTagHelperTests()
+        [Fact]
+        public void Can_Generate_Page_Links()
         {
+
+            // Arrange
+
         }
     }
 }
