521773219fd6601bd0a71bc9306702311609fd3b
Vasilisdm <vasilisdmr@gmail.com>
2019-06-20 23:51:14 +0300

    Finished the arrange part of the test.

--- SportsStore.Tests/PageLinkTagHelperTests.cs
+++ SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 //using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moq;
 using SportsStore.Infrastructure;
@@ -15,9 +16,38 @@ namespace SportsStore.Tests
         [Fact]
         public void Can_Generate_Page_Links()
         {
-
             // Arrange
+            var urlHelper = new Mock<IUrlHelper>();
+            urlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
+                .Returns("Test/Page1")
+                .Returns("Test/Page2")
+                .Returns("Test/Page3");
 
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+
+            urlHelperFactory.Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>())).Returns(urlHelper.Object);
+
+            PageLinkTagHelper helper = new PageLinkTagHelper(urlHelperFactory.Object)
+            {
+                PageModel = new PagingInfo
+                {
+                    CurrentPage = 2,
+                    TotalItems = 28,
+                    ItemsPerPage = 10
+                },
+                PageAction = "Test"
+            };
+
+            TagHelperContext ctx = new TagHelperContext
+            (
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), ""
+            );
+
+            var content = new Mock<TagHelperContent>();
+
+            TagHelperOutput output = new TagHelperOutput("div", new TagHelperAttributeList(),
+                                                                (cache, encoder) => Task.FromResult(content.Object));
         }
     }
 }
