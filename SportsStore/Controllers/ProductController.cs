c7ef42efdc49f074d41682e4b3bec966daadd1f9
Vasilisdm <vasilisdmr@gmail.com>
2019-06-22 16:52:25 +0300

    Passed view model to the view.

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -15,9 +16,17 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult List(int productPage = 1) => 
-                                View(_repository.Products
-                                                .OrderBy(p => p.ProductID)
-                                                .Skip((productPage - 1) * PageSize)
-                                                .Take(PageSize));
+                                View(new ProductsListViewModel {
+                                    Products = _repository.Products
+                                               .OrderBy(p => p.ProductID)
+                                               .Skip((productPage -1 ) * PageSize)
+                                               .Take(PageSize),
+                                    PagingInfo = new PagingInfo
+                                    {
+                                        CurrentPage = productPage,
+                                        ItemsPerPage = PageSize,
+                                        TotalItems = _repository.Products.Count()
+                                    }
+                                });
     }
 }
