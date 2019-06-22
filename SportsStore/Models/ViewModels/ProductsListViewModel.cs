65ad85de3a12ecffc356655c8faf1c8643c79556
Vasilisdm <vasilisdmr@gmail.com>
2019-06-22 16:48:30 +0300

    Corrected a typo.

--- SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -6,6 +6,6 @@ namespace SportsStore.Models.ViewModels
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
-        public PagingInfo PagingInfor { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
 }
