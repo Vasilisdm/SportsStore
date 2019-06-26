8aba46bfb8fc7e464e2decffd3a85d77ce9c071b
Vasilisdm <vasilisdmr@gmail.com>
2019-06-27 00:09:34 +0300

    Made some changes to the test.

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -16,18 +16,20 @@ namespace SportsStore.Tests
         {
             // Arrange
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
-
-            mock.Setup(m => m.Products).Returns(new Product[] {
+            mock.Setup(m => m.Products).Returns((new Product[] {
                 new Product {ProductID = 1, Name = "P1", Category = "Apples"},
                 new Product {ProductID = 2, Name = "P2", Category = "Apples"},
                 new Product {ProductID = 3, Name = "P3", Category = "Plums"},
                 new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
-            }.AsQueryable<Product>());
+            }).AsQueryable<Product>());
 
-            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            NavigationMenuViewComponent target =
+                new NavigationMenuViewComponent(mock.Object);
 
             // Act get the set of categories
-            string[] results = ((IEnumerable<string>)(target.Invoke() as ViewComponentResult).ViewData.Model).ToArray();
+            string[] results = ((IEnumerable<string>)(target.Invoke()
+                as ViewComponentResult).ViewData.Model).ToArray();
+
 
             // Assert
             Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, results));
