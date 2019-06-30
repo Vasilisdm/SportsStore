eb468b15fd4df4a97e38128ff9c6c7336bbae6de
Vasilisdm <vasilisdmr@gmail.com>
2019-06-30 18:04:36 +0300

    Corrected the product count per category.

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -26,7 +26,9 @@ namespace SportsStore.Controllers
                                     {
                                         CurrentPage = productPage,
                                         ItemsPerPage = PageSize,
-                                        TotalItems = _repository.Products.Count()
+                                        TotalItems =
+                                                    category == null ? _repository.Products.Count()
+                                                                     : _repository.Products.Where(p => p.Category == category).Count()
                                     },
                                     CurrentCategory = category
                                 });
