9b1875641a692a2356d2b22a487e5a86138f0463
Vasilisdm <vasilisdmr@gmail.com>
2019-07-01 23:32:31 +0300

    Added Can_Add_New_Lines test.

--- SportsStore.Tests/CartTests.cs
+++ SportsStore.Tests/CartTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using System.Linq;
+using SportsStore.Models;
+using Xunit;
+
 namespace SportsStore.Tests
 {
     public class CartTests
     {
-        public CartTests()
+        [Fact]
+        public void Can_Add_New_Lines()
         {
+            // Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart cart = new Cart();
+
+            // Act
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            CartLine[] result = cart.Lines.ToArray();
+
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.Equal(p1, result[0].Product);
+            Assert.Equal(p2, result[1].Product);
         }
     }
 }
