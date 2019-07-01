15433759daec464314e63de103caaf21c2a4336d
Vasilisdm <vasilisdmr@gmail.com>
2019-07-01 23:39:18 +0300

    Added Can_Add_Quantity_For_Existing_Lines test.

--- SportsStore.Tests/CartTests.cs
+++ SportsStore.Tests/CartTests.cs
@@ -25,5 +25,25 @@ namespace SportsStore.Tests
             Assert.Equal(p1, result[0].Product);
             Assert.Equal(p2, result[1].Product);
         }
+
+        [Fact]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart cart = new Cart();
+
+            // Act
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            cart.AddItem(p1, 20);
+            CartLine[] result = cart.Lines.OrderBy(p => p.Product.ProductID).ToArray();
+
+            // Assert
+            Assert.Equal(21, result[0].Quantity);
+            Assert.Equal(1, result[1].Quantity);
+        }
     }
 }
