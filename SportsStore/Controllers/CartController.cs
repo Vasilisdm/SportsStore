d026951537373317d88379b79f983fcdb9fb1740
Vasilisdm <vasilisdmr@gmail.com>
2019-07-02 22:40:58 +0300

    Began implementation of the CartController

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository _repository;
+        
+        public CartController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+    }
+}
