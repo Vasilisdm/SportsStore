c83382a7d0c262a9ef3e6913c6d53e74e7e51128
Vasilisdm <vasilisdmr@gmail.com>
2019-07-02 23:29:57 +0300

    Implemented GetJson extension method.

--- SportsStore/Infrastructure/SessionExtensions.cs
+++ SportsStore/Infrastructure/SessionExtensions.cs
@@ -7,7 +7,14 @@ namespace SportsStore.Infrastructure
     {
        public static void SetJson(this ISession session, string key, object value)
        {
-            session.SetString(key, JsonConvert.SerializeObject(value);
+            session.SetString(key, JsonConvert.SerializeObject(value));
        }
+
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+
+            return sessionData == null ? default(T) : JsonConvert.DeserializeObject<T>(sessionData);
+        }
     }
 }
