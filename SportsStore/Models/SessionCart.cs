711964c5dffb5a226005ba84c623900ddc523aa4
Vasilisdm <vasilisdmr@gmail.com>
2019-07-04 22:37:04 +0300

    The sessionCart model is class derived from Cart, which will be aware of how to store itseld using session state.

---
+++ SportsStore/Models/SessionCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Models
+{
+    public class SessionCart : Cart
+    {
+        public SessionCart()
+        {
+        }
+    }
+}
