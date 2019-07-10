924eaccbdda9c77cb31741eb3e981bbcd67cbd07
Vasilisdm <vasilisdmr@gmail.com>
2019-07-10 22:18:55 +0300

    Began the implementation of the order repository.

---
+++ SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        public IQueryable<Order> Orders => throw new NotImplementedException();
+
+        public void SaveOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
