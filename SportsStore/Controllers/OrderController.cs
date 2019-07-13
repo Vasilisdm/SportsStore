257d69061610843029a928ebe251abe835f4c621
Vasilisdm <vasilisdmr@gmail.com>
2019-07-13 18:23:00 +0300

    Implemented the constructor passing order repo and cart instance.

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -5,6 +5,15 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
+        private IOrderRepository _repositoy;
+        private Cart _cart;
+
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            _repositoy = repoService;
+            _cart = cartService;
+        }
+
         public ViewResult CheckOut() => View(new Order());
     }
 }
