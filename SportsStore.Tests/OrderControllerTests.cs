c60bb7a323010de8bac1eb9f6472a9ab3f23b601
Vasilisdm <vasilisdmr@gmail.com>
2019-07-15 21:27:32 +0300

    Began implementation of Cannot_Checkout_Empty_Cart test method.

---
+++ SportsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+namespace SportsStore.Tests
+{
+    public class OrderControllerTests
+    {
+      [Fact]
+        public void Cannot_Checkout_Empty_Cart()
+        {
+
+        }
+    }
+}
