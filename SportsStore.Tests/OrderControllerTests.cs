e38acef24509706efcb1d7e40ddc2d54be9b2e5b
Vasilisdm <vasilisdmr@gmail.com>
2019-07-15 22:45:47 +0300

    Finished implementation of Cannot_Checkout_Empty_Cart.

--- SportsStore.Tests/OrderControllerTests.cs
+++ SportsStore.Tests/OrderControllerTests.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
 using Xunit;
+
 namespace SportsStore.Tests
 {
     public class OrderControllerTests
@@ -7,7 +11,26 @@ namespace SportsStore.Tests
       [Fact]
         public void Cannot_Checkout_Empty_Cart()
         {
+            // Arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
 
+            Cart cart = new Cart();
+
+            Order order = new Order();
+
+            OrderController orderCntlr = new OrderController(mock.Object, cart);
+
+            // Act
+            ViewResult result = orderCntlr.Checkout(order) as ViewResult;
+
+            // Assert - check that the order hasn't been stored
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+
+            // Assert - check that the method is returning the default view
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+
+            // Assert - check that I am passing an invalid model to the view
+            Assert.False(result.ViewData.ModelState.IsValid);
         }
     }
 }
