4e81c4fc37e9e26ee7948d6501d30c9857f19cb4
Vasilisdm <vasilisdmr@gmail.com>
2019-07-15 23:21:05 +0300

    Began implementation of Can_Checkout_And_Submit_Order.

--- SportsStore.Tests/OrderControllerTests.cs
+++ SportsStore.Tests/OrderControllerTests.cs
@@ -37,6 +37,34 @@ namespace SportsStore.Tests
         [Fact]
         public void Cannot_Checkout_Invalid_ShippingDetails()
         {
+            // Assert
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+
+            OrderController orderCntrl = new OrderController(mock.Object, cart);
+
+            orderCntrl.ModelState.AddModelError("error", "error");
+
+            // Act - Try to check out
+            ViewResult result = orderCntrl.Checkout(new Order()) as ViewResult;
+
+            // Assert
+            // Check that the order hasn't been stored
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+
+            // Check that the method is returning the default view
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+
+            // Check that I am passing an invalid model to the view
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+
+        [Fact]
+        public void Can_Checkout_And_Submit_Order()
+        {
 
         }
     }
