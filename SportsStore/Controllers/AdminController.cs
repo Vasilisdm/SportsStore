d7952a979df7a2642db9b1acd0332988831df895
Vasilisdm <vasilisdmr@gmail.com>
2019-07-16 22:16:27 +0300

    Added AdminController and implemented the index method.

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult Index() => View(_repository.Products);
+    }
+}
