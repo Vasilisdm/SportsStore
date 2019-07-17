9310add65aca043f13ba0faff08a9e6d50bd0fe2
Vasilisdm <vasilisdmr@gmail.com>
2019-07-17 20:34:45 +0300

    Implemented Can_Edit_Product test method.

--- SportsStore.Tests/AdminControllerTests.cs
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -35,6 +35,31 @@ namespace SportsStore.Tests
             Assert.Equal("P3", result[2].Name);
         }
 
+        [Fact]
+        public void Can_Edit_Product()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"}
+            }.AsQueryable<Product>());
+
+            AdminController cntrl = new AdminController(mock.Object);
+
+            // Act
+            Product p1 = GetViewModel<Product>(cntrl.Edit(1));
+            Product p2 = GetViewModel<Product>(cntrl.Edit(2));
+            Product p3 = GetViewModel<Product>(cntrl.Edit(3));
+
+            // Assert
+            Assert.Equal(1, p1.ProductID);
+            Assert.Equal(2, p2.ProductID);
+            Assert.Equal(3, p3.ProductID);
+        }
+
         private T GetViewModel<T>(IActionResult result) where T : class
         {
             return (result as ViewResult)?.ViewData.Model as T;
