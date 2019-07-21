613a9846293d3075836c91bec7dc34125be316a1
vasilisdimitriou <vasilisdmr@gmail.com>
2019-07-21 16:22:06 +0300

    Implemented the SaveProduct method.

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -6,11 +6,35 @@ namespace SportsStore.Models
     {
         private ApplicationDbContext _context;
 
+
         public EFProductRepository(ApplicationDbContext ctx)
         {
             _context = ctx;
         }
 
         public IQueryable<Product> Products => _context.Products;
+
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                _context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = _context.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+
+            _context.SaveChanges();
+        }
     }
 }
