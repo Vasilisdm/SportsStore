aa86287f1995c47c5f277e76a61263146f274eaa
vasilisdimitriou <vasilisdmr@gmail.com>
2019-07-21 16:55:15 +0300

    Added an overloaded edit method.

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -16,5 +16,21 @@ namespace SportsStore.Controllers
         public ViewResult Index() => View(_repository.Products);
 
         public ViewResult Edit(int productId) => View(_repository.Products.FirstOrDefault(p => p.ProductID == productId));
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
+        }
     }
 }
